06504db35b9c3760861ceb68c00661edf488e78b
Anand M S <anandagireesh@gmail.com>
2024-03-04 17:16:27 +0530

    updated forget password

--- app/Livewire/Admin/Section/FailedMessage.php
+++ app/Livewire/Admin/Section/FailedMessage.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Admin\Section;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class FailedMessage extends Component
 {
+    #[Layout('components.layouts.error')]
     public function render()
     {
         return view('livewire.admin.section.failed-message');
